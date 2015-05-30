178899eff3444351141c5cf76f0844ce43519de2
DruzhyninB <druzhyninbogdan@gmail.com>
2015-05-30 05:55:32 +0300

    Я надеюсь финальное постфинальное изменение
    мы поменяли все что могли

--- js/portfolio_slider.js
+++ js/portfolio_slider.js
@@ -8,173 +8,95 @@ $("#elastic_grid_demo").elastic_grid({
 	'items' :
 		[
 			{
-			'title' : 'Friends',
-			'thumbnail': ['img/portfolio/small/cat_dog_small.jpg', 'img/portfolio/small/cat_fish_small.jpg'],
-			'large': ['img/portfolio/large/cat_dog_large.jpg', 'img/portfolio/large/cat_fish_large.jpg', ],
+			'title' : 'Митти и Джеми',
+			'thumbnail': ['img/portfolio/small/cat_dog_small.jpg', 'img/portfolio/small/1_1.jpg','img/portfolio/small/1.jpg'],
+			'large': ['img/portfolio/large/cat_dog_large.jpg', 'img/portfolio/large/1.jpg','img/portfolio/large/1_2.jpg' ],
 			'button_list'   :
 		     [
 			 ],
 			'tags'  : ['Все']
 			},
-
             {
-                'title': 'Friends',
-                'thumbnail': ['img/portfolio/small/dog_bird_small.jpg', 'img/portfolio/small/dog_5_small.jpg'],
-                'large': ['img/portfolio/large/dog_bird_large.jpg', 'img/portfolio/large/dog_5_large.jpg', ],
+                'title': 'Роксар и Хич',
+                'thumbnail': ['img/portfolio/small/dog_bird_small.jpg', 'img/portfolio/small/2.jpg','img/portfolio/small/2_1.jpg'],
+                'large': ['img/portfolio/large/dog_bird_large.jpg', 'img/portfolio/large/2_1.jpg','img/portfolio/large/2.jpg' ],
                 'button_list':
                 [
                 ],
                 'tags': ['Все']
             },
-
 			{
-			'title' : 'Swiss chard pumpkin',
-			'thumbnail' : ['img/portfolio/small/dog_1_small.jpg', 'img/portfolio/small/dog_2_small.jpg'],
-			'large' : ['img/portfolio/large/dog_1_large.jpg', 'img/portfolio/large/dog_2_large.jpg'],
+			    'title': 'Глория',
+			'thumbnail' : ['img/portfolio/small/dog_1_small.jpg', 'img/portfolio/small/3.jpg','img/portfolio/small/3_1.jpg'],
+			'large' : ['img/portfolio/large/dog_1_large.jpg', 'img/portfolio/large/3_2.jpg','img/portfolio/large/3_1.jpg'],
 			'button_list'   :
 			[
 			],
 			'tags'  : ['Собаки','Все']
 			},
-
 			{
-			'title' : 'Spinach winter purslane',
-			'description'   : 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.',
-			'thumbnail' : ['img/portfolio/small/cat_1_small.jpg','img/portfolio/small/cat_2_small.jpg'],
-			'large' : ['img/portfolio/large/cat_1_large.jpg','img/portfolio/large/cat_2_large.jpg'],
+			    'title': 'Грей',
+			'thumbnail' : ['img/portfolio/small/cat_1_small.jpg','img/portfolio/small/4.jpg','img/portfolio/small/4_1.jpg'],
+			'large': ['img/portfolio/large/cat_1_large.jpg', 'img/portfolio/large/4_1.jpg', 'img/portfolio/large/4.jpg'],
 			'button_list'   :
 			[
-			{ 'title':'Demo', 'url' : 'http://#' },
-			{ 'title':'Download', 'url':'http://#'}
 			],
 			'tags'  : ['Кошки', 'Все']
 			},
-
 			{
-			'title' : 'Aubergine napa cabbage',
-			'description'   : 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.',
-			'thumbnail' : ['img/portfolio/small/fish_1_small.jpg', 'img/portfolio/small/fish_2_small.jpg'],
-			'large': ['img/portfolio/large/fish_1_large.jpg', 'img/portfolio/large/fish_2_large.jpg'],
+			    'title': 'Фолли',
+			    'thumbnail': ['img/portfolio/small/fish_2_small.jpg', 'img/portfolio/small/5_1.jpg','img/portfolio/small/5.jpg'],
+			    'large': ['img/portfolio/large/fish_2_large.jpg', 'img/portfolio/large/5.jpg','img/portfolio/large/5_1.jpg'],
 			'button_list'   :
 			[
-			{ 'title':'Demo', 'url' : 'http://#' },
-			{ 'title':'Download', 'url':'http://#'}
 			],
 			'tags'  : ['Рыбки','Все']
 			},
-
 			{
-			'title' : 'Swiss chard pumpkin',
-			'description'   : 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.',
-			'thumbnail': ['img/portfolio/small/bird_1_small.jpg', 'img/portfolio/small/bird_2_small.jpg'],
-			'large': ['img/portfolio/large/bird_1_large.jpg', 'img/portfolio/large/bird_2_large.jpg'],
+			    'title': 'Джордж',
+			    'thumbnail': ['img/portfolio/small/bird_2_small.jpg', 'img/portfolio/small/6.jpg','img/portfolio/small/6_1.jpg'],
+			    'large': ['img/portfolio/large/bird_2_large.jpg', 'img/portfolio/large/6_1.jpg','img/portfolio/large/6.jpg'],
 			'button_list'   :
 			[
-			{ 'title':'Demo', 'url' : 'http://#' },
-			{ 'title':'Download', 'url':'http://#'}
 			],
 			'tags'  : ['Птицы','Все']
 			},
-
 			{
-			'title' : 'Spinach winter purslane',
-			'description'   : 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.',
-			'thumbnail' : ['img/portfolio/small/dog_3_small.jpg','img/portfolio/small/dog_4_small.jpg'],
-			'large' : ['img/portfolio/large/dog_3_large.jpg','img/portfolio/large/dog_4_large.jpg'],
+			    'title': 'Дана',
+			'thumbnail' : ['img/portfolio/small/dog_3_small.jpg','img/portfolio/small/7_1.jpg','img/portfolio/small/7.jpg'],
+			'large' : ['img/portfolio/large/dog_3_large.jpg','img/portfolio/large/7.jpg','img/portfolio/large/7_1.jpg'],
 			'button_list'   :
 			[
-			{ 'title':'Demo', 'url' : 'http://#' },
-			{ 'title':'Download', 'url':'http://#'}
 			],
 			'tags': ['Собаки', 'Все']
 			},
-
 			{
-			'title' : 'Spinach winter purslane',
-			'description'   : 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.',
-			'thumbnail' : ['img/portfolio/small/cat_3_small.jpg','img/portfolio/small/cat_4_small.jpg'],
-			'large' : ['img/portfolio/large/cat_3_large.jpg','img/portfolio/large/cat_4_large.jpg'],
+			    'title': 'Херши',
+			'thumbnail' : ['img/portfolio/small/cat_3_small.jpg','img/portfolio/small/8.jpg','img/portfolio/small/8_1.jpg'],
+			'large' : ['img/portfolio/large/cat_3_large.jpg','img/portfolio/large/8_1.jpg','img/portfolio/large/8.jpg'],
 			'button_list'   :
 			[
-			{ 'title':'Demo', 'url' : 'http://#' },
-			{ 'title':'Download', 'url':'http://#'}
 			],
 			'tags': ['Кошки', 'Все']
 			},
-
 			{
-			'title' : 'Azuki bean',
-			'description'   : 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.',
-			'thumbnail' : ['img/portfolio/small/fish_3_small.jpg', 'img/portfolio/small/fish_4_small.jpg'],
-			'large' : ['img/portfolio/large/fish_3_large.jpg', 'img/portfolio/large/fish_4_large.jpg'],
+			    'title': 'Мик',
+			'thumbnail' : ['img/portfolio/small/fish_3_small.jpg', 'img/portfolio/small/9.jpg','img/portfolio/small/9_1.jpg'],
+			'large' : ['img/portfolio/large/fish_3_large.jpg', 'img/portfolio/large/9_1.jpg','img/portfolio/large/9.jpg'],
 			'button_list'   :
 			[
-			{ 'title':'Demo', 'url' : 'http://#' },
-			{ 'title':'Download', 'url':'http://#'}
 			],
 			'tags': ['Рыбки', 'Все']
 			},
-
 			{
-			'title' : 'Swiss chard pumpkin',
-			'description'   : 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.',
-			'thumbnail' : ['img/portfolio/small/bird_3_small.jpg', 'img/portfolio/small/bird_4_small.jpg'],
-			'large' : ['img/portfolio/large/bird_3_large.jpg', 'img/portfolio/large/bird_4_large.jpg'],
+			    'title': 'Фаэр',
+			'thumbnail' : ['img/portfolio/small/bird_3_small.jpg', 'img/portfolio/small/10_1.jpg','img/portfolio/small/10.jpg'],
+			'large' : ['img/portfolio/large/bird_3_large.jpg', 'img/portfolio/large/10.jpg','img/portfolio/large/10_1.jpg'],
 			'button_list'   :
 			[
-			{ 'title':'Demo', 'url' : 'http://#' },
-			{ 'title':'Download', 'url':'http://#'}
 			],
 			'tags': ['Птицы', 'Все']
 			},
-            {
-                'title': 'TEST',
-                'description': 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.',
-                'thumbnail': ['img/portfolio/small/bird_3_small.jpg', 'img/portfolio/small/bird_4_small.jpg'],
-                'large': ['img/portfolio/large/bird_3_large.jpg', 'img/portfolio/large/bird_4_large.jpg'],
-                'button_list':
-                [
-                { 'title': 'Demo', 'url': 'http://#' },
-                { 'title': 'Download', 'url': 'http://#' }
-                ],
-                'tags': ['Собаки', 'Все']
-            },
-            {
-                'title': 'TEST',
-                'description': 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.',
-                'thumbnail': ['img/portfolio/small/bird_3_small.jpg', 'img/portfolio/small/bird_4_small.jpg'],
-                'large': ['img/portfolio/large/bird_3_large.jpg', 'img/portfolio/large/bird_4_large.jpg'],
-                'button_list':
-                [
-                { 'title': 'Demo', 'url': 'http://#' },
-                { 'title': 'Download', 'url': 'http://#' }
-                ],
-                'tags': ['Кошки', 'Все']
-            },
-            {
-                'title': 'TEST',
-                'description': 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.',
-                'thumbnail': ['img/portfolio/small/bird_3_small.jpg', 'img/portfolio/small/bird_4_small.jpg'],
-                'large': ['img/portfolio/large/bird_3_large.jpg', 'img/portfolio/large/bird_4_large.jpg'],
-                'button_list':
-                [
-                { 'title': 'Demo', 'url': 'http://#' },
-                { 'title': 'Download', 'url': 'http://#' }
-                ],
-                'tags': ['Птицы', 'Все']
-            },
-            {
-                'title': 'TEST',
-                'description': 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.',
-                'thumbnail': ['img/portfolio/small/bird_3_small.jpg', 'img/portfolio/small/bird_4_small.jpg'],
-                'large': ['img/portfolio/large/bird_3_large.jpg', 'img/portfolio/large/bird_4_large.jpg'],
-                'button_list':
-                [
-                { 'title': 'Demo', 'url': 'http://#' },
-                { 'title': 'Download', 'url': 'http://#' }
-                ],
-                'tags': ['Рыбки', 'Все']
-            },
 		]
 	});
 });
